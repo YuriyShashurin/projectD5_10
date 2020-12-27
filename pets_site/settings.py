42cae6dc9ee559423bc34eb6f8126210d05a2c5c
Yury Shashurin <shashurin.edu@mail.ru>
2020-12-27 19:36:03 +0300

    about to deploy 4

--- pets_site/settings.py
+++ pets_site/settings.py
@@ -79,7 +79,12 @@ WSGI_APPLICATION = 'pets_site.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))}
-
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
