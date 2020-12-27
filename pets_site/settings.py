f9b4906fe48008ad94e1c1a9898d97f4a0c21969
Yury Shashurin <shashurin.edu@mail.ru>
2020-12-27 19:20:20 +0300

    about to deploy

--- pets_site/settings.py
+++ pets_site/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url  
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ice=g+)9cdcx9ih)c0o7bbcp#mj!cp*&s%(a*0^#l5w5fmt!5^'
+# SECRET_KEY = 'ice=g+)9cdcx9ih)c0o7bbcp#mj!cp*&s%(a*0^#l5w5fmt!5^'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -76,12 +78,7 @@ WSGI_APPLICATION = 'pets_site.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+DATABASES = {'default': dj_database_url.config(default=os.environ.get('DATABASE_URL'))}
 
 
 # Password validation
